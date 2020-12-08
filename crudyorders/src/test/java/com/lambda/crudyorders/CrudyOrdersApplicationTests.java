bd9abcafd2387c0299096dba30175d7764655273
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-12-07 20:39:00 -0500

    basic project setup

---
+++ crudyorders/src/test/java/com/lambda/crudyorders/CrudyOrdersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambda.crudyorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudyOrdersApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
